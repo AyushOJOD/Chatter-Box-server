4cc58a2b3f488dab34129ebb5b816e53f1b3f186
Ayush <rishiustaad@gmail.com>
2024-01-17 22:51:58 +0530

    implemented real-time chat using socket.io

--- index.js
+++ index.js
@@ -19,3 +19,26 @@ const server = app.listen(process.env.PORT, () => {
 });
 
 connectDB;
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-receive", data.message);
+    }
+  });
+});
